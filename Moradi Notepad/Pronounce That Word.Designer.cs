cfd3828c6a8f83259f06da7286661ca3fd0c77cc
hudson1740 <brett@brett-techrepair.com>
2018-02-23 17:52:42 -0500

    Added window icon to word pronouncer

--- Moradi Notepad/Pronounce That Word.Designer.cs
+++ Moradi Notepad/Pronounce That Word.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pronounce_That_Word));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -93,6 +94,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Pronounce_That_Word";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
