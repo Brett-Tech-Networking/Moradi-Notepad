07deeccc39a9bf69695bc8a85b15521cde6a3768
matthew17754 <matmoradi17@gmail.com>
2018-02-22 21:40:09 -0800

    Removed Unnecessary Align Classes
    Not sure why these are here. Everything seems to work ok without them so I am wiping them from the face of the earth.

--- Moradi Notepad/menuStrip_Form_Align_Left.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Forms;
-
-namespace Moradi_Notepad
-{
-    internal class menuStrip_Form_Align_Left
-    {
-        public static CheckState CheckState { get; internal set; }
-    }
-}
--- Moradi Notepad/toolStrip_Down_AlignJustify.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Forms;
-
-namespace Moradi_Notepad
-{
-    internal class toolStrip_Down_AlignJustify
-    {
-        public static CheckState CheckState { get; internal set; }
-    }
-}
